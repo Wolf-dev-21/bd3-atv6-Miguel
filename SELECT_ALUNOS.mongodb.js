6031310ec1bf69e0cce61bef359a1733ec4bd037
Wolf-dev-21 <wolf.of.art21@gmail.com>
2024-06-14 19:48:42 -0300

    completa

--- SELECT_ALUNOS.mongodb.js
+++ SELECT_ALUNOS.mongodb.js
@@ -1,3 +1,7 @@
-use('lBD3-NoSQL-AtlasMongoDB');
+use('BD3-NoSQL-AtlasMongoDB');
 /* SELECIONA TODOS OS DADOS DE TODOS OS ARQUIVOS DA COLLECTION */
 db['bd3-nosql-atv6'].find();
+
+//Por CPF
+use('BD3-NoSQL-AtlasMongoDB');
+db['bd3-nosql-atv6'].find({"cpf":48061733803}, {"_id":0,});
